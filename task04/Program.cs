d40e6319d42b1322c802ea039d370e1a97cb1424
dmyk <dmyk@nuuday.dk>
2021-04-14 17:38:57 +0300

    Small formating fix

--- task04/Program.cs
+++ task04/Program.cs
@@ -9,6 +9,7 @@ namespace task04
             Figure figure1 = new(new Point(1, 1, "triangle"), new Point(2, 2, ""), new Point(3, 3, ""));
             Figure figure2 = new(new Point(1, 1, "rectangle"), new Point(2, 2, ""), new Point(3, 3, ""), new Point(4, 4, ""));
             Figure figure3 = new(new Point(1, 1, "pentagon"), new Point(2, 2, ""), new Point(3, 3, ""), new Point(4, 4, ""), new Point(5, 5, ""));
+
             Console.WriteLine(figure1.Name);
             Figure.PerimeterCalculator(figure1);
             Console.WriteLine(figure2.Name);
